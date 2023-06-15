6997c12df95f4e93e9dbf1c273d40c11cae52f07
mcnsgit <cardigamiguel221@gmail.com>
2023-06-15 22:22:30 +0100

    .

---
+++ my_app/backend/src/controllers/authController.js
@@ -0,0 +1,9 @@
+const User = require('../models/user');
+
+exports.register = async (req, res) => {
+  // Registration logic here
+};
+
+exports.login = async (req, res) => {
+  // Login logic here
+};
