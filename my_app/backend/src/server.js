83993c722b767aabccd85080a2b00e6a72fea002
mcnsgit <cardigamiguel221@gmail.com>
2023-06-18 19:18:42 +0100

    .

--- my_app/backend/src/server.js
+++ my_app/backend/src/server.js
@@ -1,1 +1,23 @@
-const express = require('express');
+// db.js
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://cardigamiguel221:Dropshiping22@mcnsblogtest.ngpfkjm.mongodb.net/',
+{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+    // pusher.js
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+    appId: 'your-app-id',
+    key: 'your-key',
+    secret: 'your-secret',
+    cluster: 'your-cluster',
+    useTLS: true
+});
+
+// Trigger an event on a channel
+pusher.trigger('my-channel', 'my-event', {
+    message: 'hello world'
+});
--- my_app/frontend/src/screens/MessageScreen.js
+++ my_app/frontend/src/screens/MessageScreen.js
@@ -1,1 +1,26 @@
-import React from 'react';
+// MessagesPage.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+
+const MessagesPage = ({ messages }) => {
+    const [newMessage, setNewMessage] = useState('');
+
+    const handleSend = () => {
+    // Send new message to backend
+    sendMessage(newMessage).then(() => setNewMessage(''));
+    };
+
+    return (
+    <View>
+        {messages.map(message => (
+        <View key={message.id}>
+            <Text>{message.content}</Text>
+        </View>
+        ))}
+        <TextInput value={newMessage} onChangeText={setNewMessage} />
+        <Button title="Send" onPress={handleSend} />
+    </View>
+    );
+};
+
+export default MessagesPage;
